613112655dbcbcada029f9345b8b2e85f0ad8dfc
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-14 13:47:11 +0200

    added newer version from codepen

--- script.js
+++ script.js
@@ -7,50 +7,39 @@ var currency = {
   czk: 0.0424931,
   gbp: 1.26376,
   inr: 0.0133285,
-  jpy: 0.00931207,
+  jpy: 0.00931207
 };
-// var currency = ["usd", "eur", "czk", "gbp", "inr", "jpy"];
-// var currencyRatio = [1, 1.13194, 0.0424931, 1.26376, 0.0133285, 0.00931207];
-// var ratioFrom = currencyRatio[0];
+
 var ratioFrom = currency.usd;
-// var ratioTo = currencyRatio[0];
+
 var ratioTo = currency.usd;
 var amount = 1;
 
 function input() {
   amount = document.getElementById("amount").value;
-  console.log(amount);
 }
 
 function from() {
   let currencySelected = currencyFrom.value;
-  // ratioFrom = currencyRatio[currency.indexOf(currencySelected)];
   ratioFrom = currency[`${currencySelected}`];
-  console.log("from:", currencySelected, ratioFrom);
 }
 
 function to() {
   let currencySelected = currencyTo.value;
-  // ratioTo = currencyRatio[currency.indexOf(currencySelected)];
   ratioTo = currency[`${currencySelected}`];
-  console.log("to:", currencySelected, ratioTo);
 }
 
 function calculation() {
   let result = amount * (ratioFrom / ratioTo);
-  console.log(result);
-  display.innerHTML = `${result.toFixed(4)}`;
+  display.innerHTML = `${amount} ${currencyFrom.value} = ${result.toFixed(4)} ${currencyTo.value}`;
 }
 
 function change() {
   // using of destructuring of array on swapping values of two variables :)
   [currencyTo.value, currencyFrom.value] = [
     currencyFrom.value,
-    currencyTo.value,
+    currencyTo.value
   ];
-  // let switching = [currencyFrom.value, currencyTo.value];
-  // currencyFrom.value = switching[1];
-  // currencyTo.value = switching[0];
   from();
   to();
 }
